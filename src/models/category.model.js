f731f9d1845602c7a3556a94b1f5006748248c20
arpitguna <dhavalleelawala@gmail.com>
2025-08-02 15:16:24 +0530

    update

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -3,17 +3,14 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
     name: {
         type: String,
-<<<<<<< HEAD
         required: true
     },
     description: {
         type: String,
         required: true
-=======
     },
     description: {
         type: String,
->>>>>>> 2058f257787d24d22b88427cb73ab0f1f7702b82
     },
     created_at: {
         type: String
