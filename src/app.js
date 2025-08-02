de2a0f4ccee9a4594dd0aec84dd1edb991837825
Girish Gondaliya <gkgondaliya23@gmail.com>
2025-08-02 11:52:23 +0530

    structue - girish

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const port = process.env.PORT || 8001;
-
 const dbConnection = require("./config/dbConfig");
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded())
+app.use(express.json())
+
+
+app.use("/api", require('./routes'))
 app.listen(port, (err) => {
   if (err) {
     console.log("Error : ", err);
