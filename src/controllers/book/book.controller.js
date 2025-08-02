dc4980469e5879fccaf002579fb8620ad9877ceb
SJaynesh <jayneshsodvadiya@gmail.com>
2025-08-02 13:14:58 +0530

    Book Route - Jaynesh Sarkar

---
+++ src/controllers/book/book.controller.js
@@ -0,0 +1,24 @@
+// Add New Book
+export const addBook = (req, res) => {
+
+}
+
+// Fetch All Books
+export const fetchBooks = (req, res) => {
+
+}
+
+// Fetch Single Book
+export const fetchSingleBook = (req, res) => {
+
+}
+
+// Update Book
+export const updateBook = (req, res) => {
+
+}
+
+// Delete Book
+export const deleteBook = (req, res) => {
+
+}
--- src/routes/book/book.route.js
+++ src/routes/book/book.route.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const route = express.Router();
 
-route.post('/')
+
+const { addBook, fetchBooks, fetchSingleBook, updateBook, deleteBook } = require("../../controllers/book/book.controller");
+
+// Add New Book
+route.post("/", addBook);
+
+
+
+// View All Books
+route.get("/", fetchBooks);
+
+// View Single Book
+route.get("/:id", fetchSingleBook);
+
+// Update Book
+route.patch("/", updateBook);
+
+// Delete Book
+route.delete("/", deleteBook)
 
 module.exports = route;
